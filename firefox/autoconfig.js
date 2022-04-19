559c0e65f2089bb81d6a3e3e7ef54b638f91db0d
alexogeny <6896115+alexogeny@users.noreply.github.com>
2022-04-19 10:59:19 +1000

    Add comment to first line of autoconfig.js

--- firefox/autoconfig.js
+++ firefox/autoconfig.js
@@ -1,3 +1,4 @@
+// Firefox AutoConfig.js
 pref('general.config.filename', 'firefox.cfg')
 pref('general.config.obscure_value', 0)
 pref('autoadmin.global_config_url', 'https://raw.githubusercontent.com/alexogeny/freckles/prime/firefox/lockedConfig.js')
