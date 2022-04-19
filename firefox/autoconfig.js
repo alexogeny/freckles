202c0dca1f7ae3a4d175339843a4e31bbcd3aac1
alexogeny <6896115+alexogeny@users.noreply.github.com>
2022-04-19 11:02:39 +1000

    Move autoadmin loader to firefox.cfg

--- firefox/autoconfig.js
+++ firefox/autoconfig.js
@@ -1,4 +1,3 @@
 // Firefox AutoConfig.js
 pref('general.config.filename', 'firefox.cfg')
 pref('general.config.obscure_value', 0)
-pref('autoadmin.global_config_url', 'https://raw.githubusercontent.com/alexogeny/freckles/prime/firefox/lockedConfig.js')
