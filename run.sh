a89babcfb5f13f45c07ba41c95c811e51b6265a8
alexogeny <6896115+alexogeny@users.noreply.github.com>
2022-04-24 23:18:47 +1000

    Remove old setup script

--- run.sh
+++
@@ -1,9 +0,0 @@
-# remove libreoffice from pop!_os/ubuntu since I have no need for office software
-sudo apt remove --purge libreoffice* && sudo apt clean && sudo apt autoremove
-# removes a few more related packages
-sudo apt remove --purge fonts-noto
-
-# remove unused locales
-sudo apt install localepurge
-# removes generated locales that are not "en"
-sudo rm -rI /usr/share/local/!(en)
