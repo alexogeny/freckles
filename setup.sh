915e1aacf8ee4527f0a81789be0beefef12473c9
alexogeny <6896115+alexogeny@users.noreply.github.com>
2022-05-06 19:20:32 +1000

    Refactor sh script yet again (but it works this time I swear)

--- setup.sh
+++ setup.sh
@@ -2,17 +2,32 @@
 
 set -e
 
-ROOT=$(cd $(dirname $BASH_SOURCE[0]) && pwd)
+ROOT=$(cd $(dirname $BASH_SOURCE[0]) && pwd);
 
-has_ansible=$(which ansible > /dev/null 2>&1)
+if ! [ -x "$(command -v ansible)" ]; then
+  echo "Do not have ansible. Installing..."
 
-$has_ansible || sudo apt update
-$has_ansible || sudo apt install software-properties-common python3-pip -y
-$has_ansible && echo "You already have ansible. Nice!"
+  # use AU sources
+  sudo sed -i 's|http://us.|http://au.|' /etc/apt/sources.list.d/system.sources
 
-alias python='python3'
-alias pip='pip3'
+  # update
+  sudo apt update
 
-$has_ansible || sudo pip install ansible black psutil pexpect --quiet
+  # upgrade
+  sudo apt upgrade -y
 
+  # install tools needed to build ansible
+  sudo apt install software-properties-common python3-pip -y
+
+  # alias python and pip
+  alias python='python3'
+  alias pip='pip3'
+
+  # install ansible
+  sudo pip install ansible black psutil pexpect --quiet
+else
+  echo "Already have ansible!"
+fi
+
+# run the playbook
 ansible-playbook -i "$ROOT/hosts" "$ROOT/freckles.yml" --ask-become-pass --ask-vault-pass
