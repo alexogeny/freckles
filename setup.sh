7ded182a6f863b2a65c2533c28a68b1c4d4d0a56
alexogeny <6896115+alexogeny@users.noreply.github.com>
2022-05-06 11:50:44 +1000

    Improve setup script to check for ansible properly

--- setup.sh
+++ setup.sh
@@ -4,12 +4,15 @@ set -e
 
 ROOT=$(cd $(dirname $BASH_SOURCE[0]) && pwd)
 
-builtin type -P "ansible" &> /dev/null || sudo apt update && sudo apt install software-properties-common python3-pip -y
+has_ansible=$(which ansible > /dev/null 2>&1)
 
+$has_ansible || sudo apt update
+$has_ansible || sudo apt install software-properties-common python3-pip -y
+$has_ansible && echo "You already have ansible. Nice!"
 
 alias python='python3'
 alias pip='pip3'
 
-sudo pip install ansible black pexpect --quiet
+$has_ansible || sudo pip install ansible black psutil pexpect --quiet
 
 ansible-playbook -i "$ROOT/hosts" "$ROOT/freckles.yml" --ask-become-pass --ask-vault-pass
