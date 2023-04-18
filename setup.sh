3c20ac2d3bf8b1abfa64124efdc4afe868f5a0b6
alexogeny <6896115+alexogeny@users.noreply.github.com>
2023-04-18 12:48:38 +1000

    feat: python and node

--- setup.sh
+++ setup.sh
@@ -1,86 +1,86 @@
 #!/bin/bash
 
-# Create symlinks for git
+if ! command -v apt-get >/dev/null 2>&1; then
+    echo "This script only supports Debian-based systems."
+    exit 1
+fi
+
 cp "$(pwd)/git/.gitconfig" "${HOME}/.gitconfig"
 cp "$(pwd)/git/.gitignore" "${HOME}/.gitignore"
 cp "$(pwd)/git/.github.gitconfig" "${HOME}/.github.gitconfig"
 cp "$(pwd)/git/.gitlab.gitconfig" "${HOME}/.gitlab.gitconfig"
 
-# Create symlinks for ssh
 mkdir -p "${HOME}/.ssh"
 cp "$(pwd)/ssh/config" "${HOME}/.ssh/config"
 
-# Create symlink for .zshrc
+if ! command -v zsh >/dev/null 2>&1; then
+    echo "zsh not found, installing..."
+    ~/.spinner sudo apt-get update && sudo apt-get install -y zsh
+fi
+
 cp "$(pwd)/zsh/.zshrc" "${HOME}/.zshrc"
 mkdir -p "${HOME}/.zsh-things"
-cp "$(pwd)/zsh/.git.zsh" "${HOME}/.zsh-things/.git.zsh"
-cp "$(pwd)/zsh/.aliases.zsh" "${HOME}/.zsh-things/.aliases.zsh"
+files=("git.zsh" "aliases.zsh" "python.zsh")
+
+for file in "${files[@]}"; do
+    cp "$(pwd)/zsh/${file}" "${HOME}/.zsh-things/${file}"
+done
 cp "$(pwd)/zsh/.spinner.sh" "${HOME}/.spinner"
 
-# Set zsh as the default shell for the current user
 if [ "$(basename "$SHELL")" != "zsh" ]; then
     chsh -s "$(command -v zsh)"
     echo "zsh is now the default shell. Please restart your terminal."
 fi
 
-# source
 source "${HOME}/.zshrc"
 
-# check if curl is installed and install it if not
 if ! command -v curl >/dev/null 2>&1; then
     echo "curl not found, installing..."
-    if command -v apt-get >/dev/null 2>&1; then
-        sudo apt-get update && sudo apt-get install -y curl
-    elif command -v pacman >/dev/null 2>&1; then
-        sudo pacman -S curl
-    else
-        echo "Package manager not supported, please install curl manually."
-        exit 1
-    fi
+    sudo apt-get update && sudo apt-get install -y curl
+fi
+if ! command -v pip3 >/dev/null 2>&1; then
+    echo "pip3 not found, installing..."
+    ~/.spinner sudo apt-get update && sudo apt-get install -y python3-pip
+fi
+
+if [ ! -d "$HOME/.pyenv" ]; then
+    echo "pyenv not found, installing..."
+    curl https://pyenv.run | bash
+    echo "pyenv installed, please restart your terminal."
+    exit 1
+fi
+
+if ! command -v pipenv >/dev/null 2>&1; then
+    echo "pipenv not found, installing..."
+    ~/.spinner pip3 install --user --break-system-packages pipenv
 fi
 
 # Check if VSCode is installed and install it if not
 if ! command -v code >/dev/null 2>&1; then
     echo "VSCode not found, installing..."
-    if command -v apt-get >/dev/null 2>&1; then
-        # Ubuntu, Debian
-        curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-        sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
-        rm packages.microsoft.gpg
+    # Ubuntu, Debian
+    curl -fsSL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor >packages.microsoft.gpg
+    sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+    rm packages.microsoft.gpg
 
-        echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
+    echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
 
-        sudo apt-get update
-        sudo apt-get install -y code
-    elif command -v pacman >/dev/null 2>&1; then
-        # Arch, Manjaro
-        sudo pacman -Syu
-        git clone https://aur.archlinux.org/visual-studio-code-bin.git
-        cd visual-studio-code-bin
-        makepkg -si
-        cd ..
-        rm -rf visual-studio-code-bin
-    else
-        echo "Package manager not supported, please install VSCode manually."
-        exit 1
-    fi
+    sudo apt-get update
+    sudo apt-get install -y code
 fi
 
-
-# Create symlinks for vscode
 mkdir -p "${HOME}/.config/Code/User"
 cp "$(pwd)/vscode/settings.json" "${HOME}/.config/Code/User/settings.json"
 cp "$(pwd)/vscode/keybinds.json" "${HOME}/.config/Code/User/keybindings.json"
 
-# Install and configure zsh
-if ! command -v zsh >/dev/null 2>&1; then
-    echo "zsh not found, installing..."
-    if command -v apt-get >/dev/null 2>&1; then
-        sudo apt-get update && sudo apt-get install -y zsh
-    elif command -v pacman >/dev/null 2>&1; then
-        sudo pacman -S zsh
-    else
-        echo "Package manager not supported, please install zsh manually."
-        exit 1
-    fi
+if ! command -v node >/dev/null 2>&1; then
+    echo "node.js not found, installing..."
+    curl -sL https://deb.nodesource.com/setup_current.x | sudo -E bash -
+    ~/.spinner sudo apt-get install -qy nodejs npm
+fi
+
+if ! command -v nvm >/dev/null 2>&1; then
+    echo "nvm not found, installing..."
+    nvm_latest_version=$(curl -s https://api.github.com/repos/nvm-sh/nvm/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/${nvm_latest_version}/install.sh | bash
 fi
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,18 +1,18 @@
 #!/usr/bin/env zsh
 
-source "${HOME}/.zsh-things/.git.zsh"
-
-# check if the user has sudoed recently and if not prompt for sudo pass
-# to keep the sudo timestamp up to date
 function check_sudo() {
     if [[ $(sudo -n uptime 2>&1|grep "load"|wc -l) -eq 0 ]]; then
         sudo -v
     fi
 }
+
 function apt-update-upgrade() {
     check_sudo
     ~/.spinner sudo apt update -qq 2>/dev/null
     ~/.spinner sudo apt upgrade -qqy 2>/dev/null
 }
 
-source "${HOME}/.zsh-things/.aliases.zsh"
+source "${HOME}/.zsh-things/git.zsh"
+source "${HOME}/.zsh-things/aliases.zsh"
+source "${HOME}/.zsh-things/python.zsh"
+source "${HOME}/.zsh-things/node.zsh"
