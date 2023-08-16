e2e898c9fc7261646927f1161d92f1d590407572
alexogeny <6896115+alexogeny@users.noreply.github.com>
2023-08-17 09:06:52 +1000

    fix: use linuxbrew version of nvm dir

--- zsh/node.zsh
+++ zsh/node.zsh
@@ -1,4 +1,5 @@
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+export NVM_DIR="$HOME/.nvm"
+  [ -s "/home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh" ] && \. "/home/linuxbrew/.linuxbrew/opt/nvm/nvm.sh"
+  [ -s "/home/linuxbrew/.linuxbrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/home/linuxbrew/.linuxbrew/opt/nvm/etc/bash_completion.d/nvm"
 
 export PATH="$HOME/.bun/bin:$PATH"
