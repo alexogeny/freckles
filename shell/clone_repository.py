f5c2d892491733c13b01f5389dbad998a8c22615
alexogeny <6896115+alexogeny@users.noreply.github.com>
2024-08-29 20:37:55 +1000

    fix: handle host arg and context arg swapped in gcl

--- shell/clone_repository.py
+++ shell/clone_repository.py
@@ -14,18 +14,25 @@ def clone_repository(
     host: HostType = "github",
     context: ContextType = "private",
 ):
+    host_arg, context_arg = host, context
     if host not in get_args(HostType):
-        raise ValueError(f"host arg should be one of {get_args(HostType)}")
+        if host in get_args(ContextType):
+            context_arg = host
+        else:
+            raise ValueError(f"host arg should be one of {get_args(HostType)}")
     if context not in get_args(ContextType):
-        raise ValueError(f"context arg should be one of {get_args(ContextType)}")
+        if context in get_args(HostType):
+            host_arg = context
+        else:
+            raise ValueError(f"context arg should be one of {get_args(ContextType)}")
     home = os.path.expanduser("~")
-    local = f"{home}/{context}/{host}/{repo}"
+    local = f"{home}/{context_arg}/{host_arg}/{repo}"
     if Path(local).exists():
         raise ValueError(f"looks like {local} already exists")
     else:
         Path(local).mkdir(parents=True)
-    repo = f"git@{host}.com-{context}:{repo}.git"
-    config = f"{home}/.{context}.{host}.gitconfig"
+    repo = f"git@{host_arg}.com-{context_arg}:{repo}.git"
+    config = f"{home}/.{context_arg}.{host_arg}.gitconfig"
     run(f"git clone {repo} {local} --quiet --depth 15")
     os.chdir(local)
     run(f'git config --local include.path "{config}"')
