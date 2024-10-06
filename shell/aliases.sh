a49e1cafcab4fd021cfa483807ad588d8ee24a56
alexogeny <6896115+alexogeny@users.noreply.github.com>
2024-10-06 21:54:34 +1000

    fix: update shell aliases and functions to use --no-project for improved execution context and environment variables setup

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -10,11 +10,11 @@ function install() {
   sudo apt install $@
 }
 
-alias gcl="uv run ~/.shell/clone_repository.py"
-alias browse="uv run ~/.shell/open_repository_in_browser.py"
-alias dynamic_navigate="uv run ~/.shell/dynamic_navigate.py"
+alias gcl="uv run --no-project ~/.shell/clone_repository.py"
+alias browse="uv run --no-project ~/.shell/open_repository_in_browser.py"
+alias dynamic_navigate="uv run --no-project ~/.shell/dynamic_navigate.py"
 function nn() {
-    local target_dir=$(uv run ~/.shell/dynamic_navigate.py "$1")
+    local target_dir=$(uv run --no-project ~/.shell/dynamic_navigate.py "$1")
     if [ -n "$target_dir" ]; then
         cd "$target_dir" || echo "Failed to change directory"
     else
@@ -42,9 +42,21 @@ alias gpl="git pull --rebase --autostash --prune"
 alias gps="git push"
 alias gpsf="git push --force-with-lease"
 
+. "$HOME/.cargo/env"
+
+export BUN_INSTALL="$HOME/.bun"
+export PATH=$BUN_INSTALL/bin:$PATH
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 get_prompt_parts() {
-  local hostname_part=$(uv run ~/.shell/hostname.py)
-  local pathname_part=$(uv run ~/.shell/pathname.py)
+  local hostname_part=$(uv run --no-project ~/.shell/hostname.py)
+  local pathname_part=$(uv run --no-project ~/.shell/pathname.py)
   echo "${hostname_part} ${pathname_part} "
 }
 
